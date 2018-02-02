d164d5b968f57496116e76bc39d018003e4b1186
pleaobraga <pleao.braga@gmail.com>
2018-02-01 22:50:00 -0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+
+const VENDOR_LIBS = [
+  'react', 
+  'lodash', 
+  'redux', 
+  'react-redux', 
+  'react-dom',
+  'redux-thunk',
+  'material-ui'
+];
+
+module.exports = {
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new OpenBrowserPlugin({ url: 'http://localhost:3333' })
+  ],
+  devServer: {
+    inline:true,
+    port: 3333
+  },
+};
