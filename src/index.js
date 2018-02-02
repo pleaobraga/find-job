f0c4a8ef27ee2116bc1b9cdc89f2ff4e1625e768
pleaobraga <pleao.braga@gmail.com>
2018-02-01 22:58:41 -0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+//import reducers from './reducers';
+
+const App = () => {
+  //const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
+  return (
+    // <Provider store={store}>
+    //   <Routes />
+    // </Provider>
+    <div>
+      Hello Word!!
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
