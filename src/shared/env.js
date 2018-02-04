cbe3ac5f5b3bf82ed353a36ae6afa184ef6789af
pleaobraga <pleao.braga@gmail.com>
2018-02-04 13:41:34 -0200

    ajust enviroments variables

--- src/shared/env.js
+++ src/shared/env.js
@@ -5,5 +5,5 @@ const hostname = window && window.location && window.location.hostname
 let env = hostname === 'find-job-pleao.herokuapp.com' ? 'PROD' : 'DEV' 
 
 export const URL_BASE = env === 'PROD' 
-    ? 'http://localhost:3000' 
+    ? 'https://job-server-pleao.herokuapp.com' 
     : 'http://localhost:3000'
